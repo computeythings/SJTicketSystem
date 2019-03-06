58d619de1643fa72a04e6ff13d986d57ccac4d86
Gonnella <bgonnella@sanjacinto.lcl>
2019-03-06 11:51:09 -0800

    formatting

--- src/index.js
+++ src/index.js
@@ -10,23 +10,25 @@ const keys = require('../lib/keys.js');
 
 passport.use(new LocalStrategy(
   async (username, password, done) => {
-  try {
+    try {
       let loginSuccess = await login(username, password);
       if (loginSuccess) {
         return done(null, username);
       }
       return done(null, false, { message: 'Incorrect username or password.' });
-  } catch(err) {
-    done(err);
-  }
-}));
+    } catch(err) {
+      done(err);
+    }
+  })
+);
 
 passport.use(new JWTStrategy({
-  jwtFromRequest: req => req.cookies.jwt,
-  secretOrKey: keys.public
-}, (jwtPayload, done) => {
-  jwt.verifyAccessToken(jwtPayload, keys.public, (err, decoded) => {
-    if (err) { return done(err.message); }
-    return done(null, jwtPayload);
-  });
-}));
+    jwtFromRequest: req => req.cookies.jwt,
+    secretOrKey: keys.public
+  }, (jwtPayload, done) => {
+    jwt.verifyAccessToken(jwtPayload, keys.public, (err, decoded) => {
+      if (err) { return done(err.message); }
+      return done(null, jwtPayload);
+    });
+  })
+);
