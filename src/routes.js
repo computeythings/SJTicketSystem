25954389e87e9b6a22348afb1815eb2d930be513
Gonnella <bgonnella@sanjacinto.lcl>
2019-03-07 12:00:42 -0800

    Adding routing

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+"use strict"
+const express = require('express');
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+
+});
+router.get('/login', (req, res) => {
+
+});
+
+router.post('/register', (req, res) => {
+
+});
+router.get('/register', (req, res) => {
+
+});
+
+router.post('/reports', (req, res) => {
+
+});
+router.get('/reports', (req, res) => {
+
+});
+
+router.post('/users', (req, res) => {
+
+});
+router.get('/users', (req, res) => {
+
+});
