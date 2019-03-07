e9710096da2cb0d00c09024a204bed3a02c44937
Gonnella <bgonnella@sanjacinto.lcl>
2019-03-07 12:01:42 -0800

    Edited for extensible use

---
+++ src/middleware.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+
+const tokens = require('../tokenhandler.js');
+const keys = require('../lib/keys.js');
+
+
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      let loginSuccess = await login(username, password);
+      if (loginSuccess) {
+        return done(null, username);
+      }
+      return done(null, false, { message: 'Incorrect username or password.' });
+    } catch(err) {
+      done(err);
+    }
+  });
+);
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: req => req.cookies.jwt,
+    secretOrKey: keys.public
+  }, (jwtPayload, done) => {
+    tokens.verifyAccessToken(jwtPayload, keys.public, (err, decoded) => {
+      if (err) { return done(err.message); }
+      return done(null, jwtPayload);
+    });
+  })
+);
+
+module.exports = passport;
