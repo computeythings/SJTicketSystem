570f1f8d48bdadde4f9832bd43724979a3238829
Gonnella <bgonnella@sanjacinto.lcl>
2019-03-14 08:53:47 -0700

    Added favicon and migrated header to seperate file

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const favicon = require('serve-favicon');
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'app/views'));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.png')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('cookie-parser')());
