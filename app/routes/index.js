70e8010605be2da62657e91e84db5bbc22e9d094
Gonnella <bgonnella@sanjacinto.lcl>
2019-03-21 10:33:45 -0700

    Added admin restricted routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,9 @@ const passport = require('passport');
 const express = require('express');
 const router = express.Router();
 
+// Routes which require admin credentials
+const RESTRICTED_ROUTES = ['/users*', '/reports/add*'];
+
 // authenticate when accessing any URL
 router.get('*', (req, res, next) => {
   // pass unauthenticated to /login
@@ -25,24 +28,25 @@ router.get('*', (req, res, next) => {
     return next();
   })(req, res, next);
 });
+// restrict access
+RESTRICTED_ROUTES.forEach((route) => {
+  console.log('setting route for', route);
+  router.get(route, (req, res, next) => {
+    passport.authenticate('jwt_admin', (err, result, data) => {
+      if (err || ! result)
+        return res.status(401).send('You do not have permission to access this.');
+      return next();
+    })(req, res, next);
+  });
+});
 
-
-
-
-/*
-{
-  session: false,
-  failureRedirect: '/login'
-}
-*/
-
-
-// authenticate when POSTing to any URL
+// restrict POSTing to any URL
 router.post('*', (req, res, next) => {
   // pass unauthenticated to /login
   if(req.url === '/login') {
     return next();
   }
+  
   passport.authenticate('jwt_admin', {
     session: false
   })(req, res, next);
